f671ef2c30899d798a8a7ec7f4482581e9eec99c
Anthony <anthonyparrett7@gmail.com>
2021-07-13 21:50:35 -0500

    Try to start without redis

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata'
 import 'dotenv-safe/config'
-import { __prod__, COOKIE_NAME } from './constants'
+// import { __prod__, COOKIE_NAME } from './constants'
+import { __prod__ } from './constants'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { UserResolver } from './resolvers/user'
-import Redis from 'ioredis'
-import session from 'express-session'
-import connectRedis from 'connect-redis'
+// import Redis from 'ioredis'
+// import session from 'express-session'
+// import connectRedis from 'connect-redis'
 import cors from 'cors'
 import { createConnection } from 'typeorm'
 import { User } from './entities/User'
@@ -29,12 +30,12 @@ const main = async () => {
 
     const app = express()
 
-    const RedisStore = connectRedis(session)
-    const redis = new Redis(process.env.REDIS_URL, {
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
+    // const RedisStore = connectRedis(session)
+    // const redis = new Redis(process.env.REDIS_URL, {
+    //     tls: {
+    //         rejectUnauthorized: false
+    //     }
+    // })
     app.set('trust proxy', 1)
     app.use(
         cors({
@@ -42,25 +43,25 @@ const main = async () => {
             credentials: true
         })
     )
-    app.use(
-        session({
-            name: COOKIE_NAME,
-            store: new RedisStore({
-                client: redis,
-                disableTouch: true
-            }),
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-                httpOnly: true,
-                sameSite: 'lax', // csrf
-                secure: __prod__, // cookie only works in https
-                domain: __prod__ ? '.heroku.com' : undefined
-            },
-            saveUninitialized: false,
-            secret: process.env.SESSION_SECRET,
-            resave: false
-        })
-    )
+    // app.use(
+        // session({
+        //     name: COOKIE_NAME,
+        //     store: new RedisStore({
+        //         client: redis,
+        //         disableTouch: true
+        //     }),
+        //     cookie: {
+        //         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        //         httpOnly: true,
+        //         sameSite: 'lax', // csrf
+        //         secure: __prod__, // cookie only works in https
+        //         domain: __prod__ ? '.heroku.com' : undefined
+        //     },
+        //     saveUninitialized: false,
+        //     secret: process.env.SESSION_SECRET,
+        //     resave: false
+        // })
+    // )
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
@@ -70,7 +71,7 @@ const main = async () => {
         context: ({ req, res }) => ({
             req,
             res,
-            redis,
+            // redis,
             userLoader: createUserLoader()
         })
     })
